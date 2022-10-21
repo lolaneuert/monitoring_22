40078bc21f91453cd6f19c84fb28a44da99f960d
lolaneuert <114755755+lolaneuert@users.noreply.github.com>
2022-10-21 17:54:52 +0200

    Update R_code_ggplot2_corona.r

--- R_code_ggplot2_corona.r
+++ R_code_ggplot2_corona.r
@@ -38,7 +38,7 @@ ggplot(corona, aes(x = virus, y = death)) + geom_polygon()
 # you can use ggplot2 to connect multiple geometric shapes simply by adding more functions with + 
 ggplot(corona, aes(x = virus, y = death)) + geom_point + geom_line + geom_polygon()
 
-# 2. now on to real data
+# 2. now on to real data: case numbers by country from the beginning of the covid-19 pandemic
 
 # recall the relevant packages using function ~library()
 library(ggplot2)
@@ -48,17 +48,36 @@ library(spatstat)
 setwd("C:/RStudio/monitoring")
 
 # now we can import the datafolder (downloaded from virtuale into wd set above) using the function ~read.table() to import data directly from the wd
-covid <- read.table("covid_agg.csv", header = TRUE)  # to import the head of the doc as a head in R aswell set the header = TRUE
+covid <- read.table("covid_agg.csv", header = TRUE)  # to import the head of the doc as a head, which doesn't contain data in R aswell, set the header = TRUE
 
 # get a first look at the data we just imported
 covid # prints the whole dataset in the console
 head(covid) # prints the first few lines of the dataset in the console
 summary(covid) # calculates the basic statistic parameters for the dataset
 
-# use function ~ggplot() to visualize the data from the set, we are using lonand lat as x and y 
+# use function ~ggplot() to visualize the data from the set, we are using lon and lat as x and y 
 ggplot(covid, aes(x = lon, y = lat)) + geom_point()
 # set the size of the points to cases so the symbol increases with case number at the different locations
 ggplot(covid, aes(x = lon, y = lat, size = cases)) + geom_point()
 
+# let's see the density of the coronavirus cases 
+# use function ~ppp() from the spatstat package to create a planar point pattern  
+attach(covid) # let's first attach the dataset to make sepcification easier (don't need $ to specify column names lat ,lon etc.)
+covid_planar <- ppp(lon, lat, c(-180, 180), c(-90, 90)) # lon and lat are x and y, function ~c() is used to tell R the range to be used
 
+# to now print a density map of the case numbers by country use function ~density()
+density_map <- density(covid_planar)
+
+plot(density_map) # simply plot the density of cases calculated above
+points(covid_planar) # add the points of the cases for each country
+
+# to visually change the map create a list of colors using function ~colorRampPalette(c())
+colors <- colorRampPalette(c('blue','yellow','orange','red','magenta'))(100)
+plot(density_map, col = colors) # now specify the color by setting col = the list created above
+points(covid_planar) # add the points of cases for each country
+
+# next we want to add the countries to the map
+# install  and activate necessary packages
+install.packages("rgdal")
+library(rgdal)
 
