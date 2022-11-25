cae29a462002a1b7290d18654c691046d15dd0c7
lolaneuert <114755755+lolaneuert@users.noreply.github.com>
2022-11-25 16:57:10 +0100

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -33,11 +33,12 @@ plot(dvi2006, col = cl)
 
 
 
+# classification of spectral images: see how many trees have been cut down in the amazon rainforest
 # classification 1992 (deforestation period d1)
 # unsupervised classification  = ...
 
 # threshold for trees in the 1992 image
-d1c <- unsuperClass(l1992, nClasses = 2) # use function ~unsuperClass() to...
+d1c <- unsuperClass(l1992, nClasses = 2) # use function ~unsuperClass() to do an unsupervised clustering of Raster* data using kmeans clustering
 plot(d1c$map)
 
 # calculate the frequencies of pixels belonging to the two classes forest and human impact in 1992
@@ -58,5 +59,32 @@ freq(d2c$map) # using function ~freq()
 # this time the two classes have switched
 # class 1: human impact: 164219
 # class 2: forest: 178507
-f2006 <- 178507/(178507+164219) # forest ratio ~ 52%
+f2006 <- 178507/(178507+164219) # forest ratio ~ 52% : decreased by 40% in less than 15 years
 h2006 <- 164219/(178507+164219) # human impact ratio ~ 48%
+
+# now compare change over time from 1992 to 2006: use function ~data.frame() to create a table containing all frequencies of forest and human impact pixels
+landcover <- c("Forest", "Humans") # first create the object columns: these are the two types
+percent_1992 <- c(90.21, 9.79) # then the percentages for the 1992 image
+percent_2006 <- c(52.08, 47.92) # and the percentages for the 2006 image
+
+proportions <- data.frame(landcover, percent_1992, percent_2006) # create a data frame containing these columns 
+proportions
+
+        
+# create a histogram plot using the ggplot2 library for the 1992 image
+hist_1992 <- ggplot(proportions, aes(x = landcover, y = percent_1992, color = landcover)) + geom_bar(stat = "identity", fill = "darkseagreen")
+hist_1992 # plot it 
+# start with function ~ggplot(), then add the name of the object that is to be used as a basis table
+# aes = aesthetics: used to specify x, y, and the colors, here x is the landcover, so either forest or human, and y is the percentage of the different covers in the 1992 image, for the color we again use the landcover variable
+# use + to add additional elements into the graph: here for histograms we need geom_bar (here specify the type of statistics, here we use "identity" so it is identical to the data in the table (not means, SE etc.), and specify the fill color
+      
+# repeat the histogram plot for the 2006 image
+hist_2006 <-  ggplot(proportions, aes(x = landcover, y = percent_2006, color = landcover)) + geom_bar(stat = "identity", fill = "darkseagreen")
+hist_2006
+
+# create a frame with both histograms together without using the function ~mfrow (multiframe) simply by using a + after activating the patchwork package
+hist_1992 + hist_2006 # shows you the two plots next to each other
+hist_1992 / hist_2006 # shows you the first plot on top of the second one
+
+# for this we could also use the function ~grid.arrange from the gridExtra package, specifying the two plots as well as the number of rows
+grid.arrange(hist_1992, hist_2006, nrow=1)
