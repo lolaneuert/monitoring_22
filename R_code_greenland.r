8d2cdf3e9496a821a0cab0acfcfacc7a3517388a
lolaneuert <114755755+lolaneuert@users.noreply.github.com>
2022-12-02 17:09:13 +0100

    Create R_code_greenland.r

---
+++ R_code_greenland.r
@@ -0,0 +1,38 @@
+# this script calculates the ice melt in island using remote sensing data
+
+# activate necessary packages
+install.packages("rasterVis")
+library(raster)
+library(ggplot2)
+library(RStoolbox)
+library(viridis)
+library(patchwork)
+library(rasterVis)
+library(rgdal)
+
+# import data using the function ~raster() (single images of land surface temperature)
+lst_2000 <- raster("lst_2000.tif")
+lst_2005 <- raster("lst_2005.tif")
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+
+# plot the images using ggplot and the viridis color palette 
+# the ~direction = parameter in viridis allows us to switch the legend color gradient when setting it to -1: here necessary, because the lower the temp, the more ice
+gg_lst_2000 <- ggplot() + geom_raster(lst_2000, mapping = aes(x = x, y = y, fill = lst_2000)) + scale_fill_viridis(option = "mako") + ggtitle("LST Greenland 2000")
+gg_lst_2005 <- ggplot() + geom_raster(lst_2005, mapping = aes(x = x, y = y, fill = lst_2005)) + scale_fill_viridis(option = "mako") + ggtitle("LST Greenland 2005")
+gg_lst_2010 <- ggplot() + geom_raster(lst_2010, mapping = aes(x = x, y = y, fill = lst_2010)) + scale_fill_viridis(option = "mako") + ggtitle("LST Greenland 2010")
+gg_lst_2015 <- ggplot() + geom_raster(lst_2015, mapping = aes(x = x, y = y, fill = lst_2015)) + scale_fill_viridis(option = "mako") + ggtitle("LST Greenland 2015")
+ 
+# the ~alpha = parameter in viridis allows us to set transparency, the closer to 0, the more transparent
+ggplot() + geom_raster(lst_2000, mapping = aes(x = x, y = y, fill = lst_2000)) + scale_fill_viridis(option = "mako", direction = 1, alpha = 0.8) + ggtitle("LST Greenland 2000")
+
+# create a multiframe with the patchwork package( or using par(mfrow = c(2,2), adding all 4 images together
+(gg_lst_2000 + gg_lst_2005) / (gg_lst_2010 + gg_lst_2015)
+ 
+# how to import/manipulate multiple images in an automated way using a loop with the function ~lapply, that applies a function over a list or vector
+# lapply returns a list of the same length as X, each element of which is the result of applying the function ~FUN to the corresponding element of X.
+# but first create a list of files using the function ~list.files(), selecting the wanted files using the parameter pattern =, which looks for elements, the wanted files have in common
+rlist <- list.files(pattern = "lst") # this creates a list of the 4 files containing the element "lst" inside their title inside the wd
+
+# the function ~lapply() can now be used on this list, specifying the list in place of x and then the wanted function that lapply should apply
+imported_lst <- lapply(rlist, raster)
