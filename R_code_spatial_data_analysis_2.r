f3a3e48966a09a4692453a1dbee0273557f5db83
lolaneuert <114755755+lolaneuert@users.noreply.github.com>
2023-01-26 17:21:02 +0100

    Update and rename R_code_ecointeract.r to R_code_spatial_data_analysis_2.r

--- R_code_spatial_data_analysis_2.r
+++ R_code_spatial_data_analysis_2.r
@@ -1,3 +1,5 @@
+
+### ecological interaction''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # this is a code for investigating relationships among ecological variables
 
 # install necessary packages using function -install.packages(""), in this case sp, used for managing spatial data 
@@ -50,7 +52,7 @@ pairs(~ cadmium + lead + zinc, data = meuse)
 
 # now you want to change the color of the graphs
 pairs(~ cadmium + lead + zinc, data = meuse, col = "darkorange2")
- 
+
 # to change the symbol in the graphs
 pairs(~ cadmium + lead + zinc, data = meuse, col = "darkorange2", pch = 23)
 
@@ -58,10 +60,10 @@ pairs(~ cadmium + lead + zinc, data = meuse, col = "darkorange2", pch = 23)
 pairs(~ cadmium + lead + zinc, data = meuse, col = "darkorange2", pch = 23, cex = 2)
 
 
-# Spatial data analysis
-
+### Spatial data analysis
 # we want to use the dataset meuse for spatial analysis
-coordinates(meuse) = ~x+y  # the function ~coordinates() either creates a spatial object, or in this case retrieves its coordinates
+# the function ~coordinates() either creates a spatial object, or in this case retrieves its coordinates
+coordinates(meuse) = ~x+y
 
 # the function ~plot() now shows how the data is spatially distributed
 plot(meuse) 
@@ -70,7 +72,7 @@ plot(meuse)
 spplot(meuse, "cadmium", main="Concentration of Cadmium") # plots the concentration of cadmium
 spplot(meuse, "copper", main="Concentration of Copper") # plots the concentration of copper
 
-# to see two variables use c (=concatenate) to attach both variables
+# to see two variables use ~c(=concatenate) to attach both variables
 spplot(meuse, c("copper","zinc"))
 
 # to create a bubble-plot of spatial data use function ~bubble(), use ~col("") to edit the color of the bubble symbol
