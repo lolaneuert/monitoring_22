f244139d4e08a18d0123399ee372a706ac947036
lolaneuert <114755755+lolaneuert@users.noreply.github.com>
2023-01-26 17:59:24 +0100

    Update and rename R_code_remote_sensing.r to 5_R_code_remote_sensing.r

--- 5_R_code_remote_sensing.r
+++ 5_R_code_remote_sensing.r
@@ -44,12 +44,12 @@ plotRGB(p224r63_2011, r = 3, g = 2, b = 4, stretch = "Lin") # band 4 is displaye
 
 # note: rectangular shapes always show human interventions!
 
-#to stretch the histogram, instead of specifying lin use hist in the function ~stretch, this stretches the values so you can look inside the forest better, for ex. detect better openings in the forest and bare soil
+# to stretch the histogram, instead of specifying lin use hist in the function ~stretch, this stretches the values so you can look inside the forest better, for ex. detect better openings in the forest and bare soil
 par(mfrow=c(2,1))
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
 
-# time series: comparison of images from 2011 and 1988
+##### time series: comparison of images from 2011 and 1988
 
 # load image from 2011 into R using the function ~brick which creates a Rasterbrick object that contains several bands of a satellite image, formed by a pixelmatrix
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
@@ -75,7 +75,7 @@ par(mfrow = c(2,1))
 plotRGB(p224r63_1988, r = 3, g = 2, b = 1, stretch = "Lin")
 plotRGB(p224r63_2011, r = 3, g = 2, b = 1, stretch = "Lin")
 
-# multi-temporal analysis: calculate the differences between images
+######## multi-temporal analysis: calculate the differences between images
 # look at the same pixel in the same image to see the change over time 
 # subtract value of one pixel in one image from the other image and assign it to the name difnir: this shows differences in the near infrared which shows us for ex. deforestation
 difnir <- p224r63_1988[[4]] - p224r63_2011[[4]]
