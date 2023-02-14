fb0f260b8adec6d460cb57f3dc55de603e450377
lolaneuert <114755755+lolaneuert@users.noreply.github.com>
2023-02-14 10:29:24 +0100

    Update 10_R_exam.r

--- 10_R_exam.r
+++ 10_R_exam.r
@@ -61,26 +61,53 @@ plot(coastlines_3035_st) # plot it to have a look
 plot_pinus_cembra <- plot(dat_pinus_cembra, pch = 20, axes = TRUE)
 plot(coastlines_3035_st, add = TRUE) # we can see the species is mainly distributed in the alps
 
+# save the plot as a pdf
+pdf("Species_distr.pdf",
+  width = 8, height = 7, # Width and height in inches
+  bg = "white",          # Background color
+  paper = "A4")
+plot_pinus_cembra <- plot(dat_pinus_cembra, pch = 20, axes = TRUE,  main = "Species distribution: Pinus Cembra")
+plot(coastlines_3035_st, add = TRUE) # we can see the species is mainly distributed in the alps
+dev.off()
+
 # we want to underlay the species distribution with a digital elevation model, this is derived from copernicus data
 dem_alps <- raster("dem.tif")
 
-# let's plot it to get a first glimpse at the data
-ggplot() + 
-  geom_raster(dem_alps, mapping = aes(x = x, y = y, fill = dem)) + scale_fill_viridis(option = "mako", direction = -1) + 
-  ggtitle("Digital Elevation Model-Alps") # we can see there are a few smaller areas of missing data in the southwestern part of the raster
+# create a color palette using the viridis color generator
+colors <- colorRampPalette(c("#fde725", "#b5de2b", "#6ece58", "#35b779", "#1f9e89", "#26828e", "#31688e", "#3e4989", "#482878", "#440154"))(4000)
+
+# plot the dem to get a first look at it
+plot(dem_alps, col = colors, main = "Digital Elevation Model - Alps", sub = "Showing species distribution and coastlines", xlab = "latitude", ylab = "longitude") 
+
+plot(dat_pinus_cembra, pch = 20, add = TRUE) # add on the species distribution data
+plot(coastlines_3035_st, add = TRUE) # add on the coastlines
+# we can see there are a few smaller areas of missing data in the southwestern part of the raster
 # as these areas only contain water anyway, we do not mind this 
-  
-# now add the species distribution data and consecutively the coastlines
-ggplot() + 
-  geom_raster(dem_alps, mapping = aes(x = x, y = y, fill = dem)) + scale_fill_viridis(option = "mako", direction = -1) + 
-  ggtitle("Digital Elevation Model-Alps") +
-  layer_spatial(dat_pinus_cembra, aes()) +
-  layer_spatial(coastlines_3035_st, aes())
-# we can see that adding the coastlines has forced ggplot to zoom out, displaying the global coastlines
+# further we see that some species occurrences lay outside of the dem raster
 
+# save the plot as pdf
+pdf("DEM.pdf",
+        width = 8, height = 7, # Width and height in inches
+        bg = "white",          # Background color
+        paper = "A4")      
+plot(dem_alps, col = colors, main = "Digital Elevation Model - Alps", sub = "Showing species distribution and coastlines", xlab = "latitude", ylab = "longitude")
+plot(dat_pinus_cembra, pch = 20, add = TRUE) # add on the species distribution data
+plot(coastlines_3035_st, add = TRUE) # add on the coastlines
+dev.off() # closes the window with the map
 
+# now we add a soil water index raster from copernicus, derived on the 01-02.07.2015
+swi_010715 <- brick("swi_010715.nc", varname = "SWI_002")# brick creates a raster object
+# looking at the dataset we see that it uses WGS84 as a projection, we need to change it to EPSG:3035, for to be compatible with the other sets
+ext_alps <- c(5, 22, 39, 52)
+swi_crop <- crop(swi_010715, ext_alps) # first we crop it to an approximate extent of the same region as the dem
+swi_3035 <- projectRaster(swi_crop, crs = "+init=epsg:3035") # now we transform the projection
+swi_crop_alps <- crop(swi_3035, dem_alps) # we can now crop it to the exact extent of the dem
 
+# let's plot it to get an impression of the data
+plot(swi_crop_alps, col = colors, main = "Soil Water Index - Alps", xlab = "latitude", ylab = "longitude")
 
+plot(dat_pinus_cembra, pch = 20, add = TRUE) # add on the species distribution data
+plot(coastlines_3035_st, add = TRUE) # add on the coastlines
 
 
 
