9301439094ce2c3248d5d235750b63339680f3db
Lola Neuert <114755755+lolaneuert@users.noreply.github.com>
2023-06-26 19:25:13 +0200

    Update 7_R_code_copernicus.r

--- 7_R_code_copernicus.r
+++ 7_R_code_copernicus.r
@@ -1,3 +1,4 @@
+### copernicus data ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # this script is on the topic of copernicus remote imaging data and how to analyse it in R
 
 # Copernicus set:
@@ -22,16 +23,19 @@ snow <- raster("C:/RStudio/monitoring/c_gls_SCE_202012210000_NHEMI_VIIRS_V1.0.1.
 snow # >200 million pixels in one layer called Snow.Cover.Extent
 
 # plot the data using ggplot and viridis # add a title
-ggplot() + geom_raster(snow, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent)) + scale_fill_viridis(option = "mako") + ggtitle("Snow Cover Extent 21.12.2020")# look at different bands using ggplot
+ggplot() + geom_raster(snow, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent)) + scale_fill_viridis(option = "mako") 
+         + ggtitle("Snow Cover Extent 21.12.2020")# look at different bands using ggplot
   
 # to have a closer look we cut the image down to a specific extention: in this case only europe should be covered
-# for this we have to specify the minimum and maximum extent to cover the specified area as elements of an array: min x~-20, max x ~70, min y ~20, max y ~75 -> this are the coordinates at the corners of our subset 
+# for this we have to specify the minimum and maximum extent to cover the specified area as elements of an array: 
+# min x~-20, max x ~70, min y ~20, max y ~75 -> this are the coordinates at the corners of our subset 
 ext <- c(-20, 70, 20, 75)
 # now use function ~crop() to cut down the image into a geographical subset
 snow_eu <- crop(snow, ext)
 
 # now plot this
-pic_snow_eu <- ggplot() + geom_raster(snow_eu, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent)) + scale_fill_viridis(option = "mako") + ggtitle("Snow Cover Extent Europe 21.12.2020") 
+pic_snow_eu <- ggplot() + geom_raster(snow_eu, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent)) 
+            + scale_fill_viridis(option = "mako") + ggtitle("Snow Cover Extent Europe 21.12.2020") 
 
 # use the + from the patchwork package to plot both images next to each other
 pic_snow + pic_snow_eu
