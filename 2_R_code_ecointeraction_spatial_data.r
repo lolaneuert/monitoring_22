4a7bb1249e1263806745ef65d2da929994c40e68
Lola Neuert <114755755+lolaneuert@users.noreply.github.com>
2023-06-26 19:15:35 +0200

    Update 2_R_code_ecointeraction_spatial_data.r

--- 2_R_code_ecointeraction_spatial_data.r
+++ 2_R_code_ecointeraction_spatial_data.r
@@ -9,12 +9,15 @@ install.packages("sp")   # "" are used everytime one leaves R environment, for e
 library(sp)
 
 # recall dataset using the function -data(), 
-data(meuse) # this is a free dataset inside of the packages sp, it is the measurements of 4 heavy metals in top soil based in Northern Europe
+data(meuse) # this is a free dataset inside of the packages sp, 
+# it is the measurements of 4 heavy metals in top soil based in Northern Europe
 # to get more info on the meuse dataset: https://cran.r.project.org/web/packages/gstat/vignettes/gstat.pdf
 
 # use functions -view(), -head() or simply the name of the dataset to look inside the dataset
-meuse # opens the complete dataset in the console(messy): it is a dataframe with 164 rows + additional columns (soil, landuse type, distance etc.)
-View(meuse) # opens the dataframe in a new window, just as if you opened it in the environment, function is from package Rcmdr, therefore install.packages("Rcmdr"), library(Rcmdr) first
+meuse # opens the complete dataset in the console(messy): it is a dataframe with 164 rows + additional columns 
+# (soil, landuse type, distance etc.)
+View(meuse) # opens the dataframe in a new window, just as if you opened it in the environment, function is from package Rcmdr, 
+# therefore install.packages("Rcmdr"), library(Rcmdr) first
 head(meuse) # opens the header and the first few rows of the dataframe in the console
 names(meuse) # gives the names of the columns of the dataframe
 # function -dev.off() is used to close opened windows
@@ -26,7 +29,8 @@ mean(meuse$cadmium) # gives you the specific mean of one column
 
 
 # 2nd exercise: plot zinc(y) against cadmium(x)
-plot(meuse$cadmium, meuse$zinc)  # in order for r to recognize the column names, you can either specify them using $ or teach him the column names beforehand
+plot(meuse$cadmium, meuse$zinc)  # in order for r to recognize the column names, you can either specify them using $ 
+# or teach R the column names beforehand
 dev.off() # to close the plot window
 
 # to teach R the name of the columns you can link the dataframe meuse with the column names camium and zinc:
@@ -40,7 +44,7 @@ plot(cadmium, zinc) # now you can simply plot c vs z and R looks in meuse for it
 # if you want to detach again use function -detach()
 
 # to create a scatterplot matrice with all the variables plotted against each other use function -pairs()
-pairs(meuse) # shows mirrored image of all 14 variables: matrix of 14 x 13: 180 potential relationships, all plotted at once, very functional
+pairs(meuse) # shows mirrored image of all 14 variables: matrix of 14 x 13: 180 potential relationships plotted at once, very functional
 
 # 2 options to pair just a few of the variables against each other
 # to create a scatterplot matrix with only the element variables cadmium, copper, lead and zinc
